17637f65aa3c73dc5af95515b058071a19a8637e
Depongkor <depongkor857@gmail.com>
2021-08-16 00:22:49 -0700

    Finished the Frontend Setup

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,111 @@
+const pizzas = [{
+
+  "name": "PEPPER BARBECUE CHICKEN",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 200,
+      "medium": 350,
+      "large": 400
+    }
+  ],
+  "category": "nonveg",
+  "image": "https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "description": "Pepper Barbecue Chicken I Cheese"
+}, {
+
+  "name": "Non Veg Supreme",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 200,
+      "medium": 350,
+      "large": 400
+    }
+  ],
+  "category": "nonveg",
+  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6q8D0F_wbH5WPjIUAOO1Yu8L3zG6mCMGDQ&usqp=CAU",
+  "description": "Bite into supreme delight of Black Olives, Onions, Grilled Mushrooms, Pepper BBQ Chicken, Peri-Peri Chicken, Grilled Chicken Rashers"
+}, {
+
+  "name": "Golden Corn Pizza",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 180,
+      "medium": 250,
+      "large": 360
+    }
+  ],
+  "category": "veg",
+  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXYdyb7DgmS8RejpYeCZH6mhSd6B7-2eBjOw&usqp=CAU",
+  "description": "Corn over the base makes it look beautiful. It is served with tomato sauce and chili flakes are sprinkled over the topping according the taste. After mixing all the ingredients, it is baked by adding cheese and corn for added flavor to pizza. Corn adds health and sweet taste to the pizza.",
+}, {
+
+  "name": "Jalapeno & Red Paprika Pizza",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 200,
+      "medium": 300,
+      "large": 420
+    }
+  ],
+  "category": "veg",
+  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM4D20JPj57TfsN1Mjr1tqrsa4bSTcifadiA&usqp=CAU",
+  "description": "This pizza is amazing and can become more delicious if we will add some more cheese in it. Ingredients are yeast, sugar, olive oil, salt, and all-purpose flour in a big bowl. After mixing all the ingredients, it is baked by adding Jalapeno and Paprika with corns over the cheese layer. The base is made crunchy to give it best taste. It can be made more tasty by sprinkling chili flakes and Oregano as per the taste."
+}, {
+
+  "name": "Margerita",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 150,
+      "medium": 220,
+      "large": 300
+    }
+  ],
+  "category": "veg",
+  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTec8zUfRIhICBszXhD7Fv7jAyTKRe7dkAYpQ&usqp=CAU",
+  "description": "The pizza base is made by mixing yeast, sugar, olive oil, salt, and all-purpose flour in a big bowl. After mixing all the ingredients, it is baked by adding the cheese as topping over it. The base is perfectly prepared by adding single layer of cheese over it. It is mouth-watering pizza for cheese lovers."
+}, {
+
+  "name": "Double Cheese Margherita Pizza",
+  "varients": [
+    "small",
+    "medium",
+    "large"
+  ],
+  "prices": [
+    {
+      "small": 250,
+      "medium": 380,
+      "large": 500
+    }
+  ],
+  "category": "veg",
+  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQplBWrr44OCkjBx87YCQVAZxYGD9f5Weyd7w&usqp=CAU",
+  "description": "This is a plain pizza which have cheese on it which is margherita and is delicious because of the loads of cheese. After mixing all the ingredients, it is baked by adding the cheese as topping over it. The base is perfectly prepared by adding double layer of cheese over it"
+}]
+
+export default pizzas;
